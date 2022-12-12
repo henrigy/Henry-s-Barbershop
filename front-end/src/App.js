efd121f4d959c0dc587d70c92f97ffd824e5b43a
Henry Liu <11.henrybliu@gmail.com>
2022-12-12 15:56:43 -0800

    titles added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,8 +2,25 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1> Henry's Barbershop</h1>
+    <div className>
+      <div className="text-5xl font-bold text-center pt-10 -mb-10">
+        <h1> HENRY'S BARBERSHOP</h1>
+      </div>
+
+      <div className="text-3xl flex justify-center">
+        <p className="m-16 relative group cursor-pointer">
+          <span>CALENDAR</span>
+          <span className="absolute -bottom-0 left-0 w-0 h-1 bg-blue-400 transition-all group-hover:w-full"></span>
+        </p>
+        <p className="m-16 relative group cursor-pointer">
+          <span>GALLERY</span>
+          <span className="absolute -bottom-0 left-0 w-0 h-1 bg-blue-400 transition-all group-hover:w-full"></span>
+        </p>
+        <p className="m-16 relative group cursor-pointer">
+          <span>REVIEWS</span>
+          <span className="absolute -bottom-0 left-0 w-0 h-1 bg-blue-400 transition-all group-hover:w-full"></span>
+        </p>
+      </div>
     </div>
   );
 };
