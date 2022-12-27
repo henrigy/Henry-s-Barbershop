e0d9824606978341c49901669bfc6d0058af00c6
Henry Liu <11.henrybliu@gmail.com>
2022-12-27 14:58:35 -0800

    initial back-end setup

---
+++ client/src/components/Reviews/Form/index.jsx
@@ -0,0 +1,69 @@
+import React, { useRef, useEffect, useCallback } from "react";
+import { useSpring, animated } from "react-spring";
+
+import {
+  Background,
+  ModalWrapper,
+  ModalContent,
+  CloseModalButton,
+} from "./FormElements";
+
+const Form = ({ showModal, setShowModal }) => {
+  const modalRef = useRef();
+
+  const animation = useSpring({
+    config: {
+      duration: 250,
+    },
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? "translateY(0%)" : "translateY(-100%)",
+  });
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === "Escape" && showModal) {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyPress);
+    return () => document.removeEventListener("keydown", keyPress);
+  }, [keyPress]);
+
+  return (
+    <>
+      {showModal ? (
+        <Background onClick={closeModal} ref={modalRef}>
+          <animated.div style={animation}>
+            <ModalWrapper showModal={showModal}>
+              <ModalContent>
+                <h1>My Review</h1>
+                <h2>Fill out name</h2>
+                <h2>
+                  Rating out of 5 Stars (Have stars that user can hover over)
+                </h2>
+                <h2>Comment</h2>
+                <button> Placeholder for now</button>
+              </ModalContent>
+              <CloseModalButton
+                aria-label="Close modal"
+                onClick={() => setShowModal((prev) => !prev)}
+              />
+            </ModalWrapper>
+          </animated.div>
+        </Background>
+      ) : null}
+    </>
+  );
+};
+
+export default Form;
