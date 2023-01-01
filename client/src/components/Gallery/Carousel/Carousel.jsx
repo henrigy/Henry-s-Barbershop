9e5d9e217f51060c7a45658b55e0a428a990d45f
Henry Liu <11.henrybliu@gmail.com>
2022-12-31 19:11:18 -0800

    update carousel

--- client/src/components/Gallery/Carousel/Carousel.jsx
+++ client/src/components/Gallery/Carousel/Carousel.jsx
@@ -1,18 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { SlArrowRight, SlArrowLeft } from 'react-icons/sl';
 import { CarouselData } from './CarouselData';
 
 const CarouselSlider = ({ slides }) => {
   const [current, setCurrent] = useState(0);
   const length = slides.length;
-
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -33,13 +24,11 @@ const CarouselSlider = ({ slides }) => {
             className={index === current ? 'slide active' : 'slide'}
             key={index}
           >
-            {index === current && (
-              <>
-                <SlArrowLeft className="left-arrow" onClick={prevSlide} />
+            <div className="slider-container">
+              {index === current ? (
                 <img src={slide.image} alt="haircut pic" className="image" />
-                <SlArrowRight className="right-arrow" onClick={nextSlide} />
-              </>
-            )}
+              ) : null}
+            </div>
           </div>
         );
       })}
